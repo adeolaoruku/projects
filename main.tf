77f05672e284e0cdca2a7d095e9c6b36ad8affc0
olaoruku007 <93499206+olaoruku007@users.noreply.github.com>
2022-07-29 06:30:11 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,15 +1,32 @@
-module "us" {
-    source = "./modules/vpc_module"
-    providers = {
-      aws = aws.us
-     }
-     vpc_cidr = "10.1.0.0/16"
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.vpc_cidr
+    enable_dns_hostnames = "true"
+    enable_dns_support = "true"
+    tags = {
+    Name = "myvpc"
+  }
 }
 
-module "europe" {
-    source = "./modules/vpc_module"
-    providers = {
-      aws = aws.europe
-     }
-     vpc_cidr = "10.1.0.0/16"
+resource "aws_internet_gateway" "myigw" {
+    vpc_id = aws_vpc.myvpc.id
+    tags = {
+    Name = "myigw"
+  }
+}
+
+resource "aws_subnet" "public" {
+    cidr_block = cidrsubnet ("${var.vpc_cidr}", 8, 1)
+    vpc_id = aws_vpc.myvpc.id
+    tags = {
+    Name = "public"
+  }
+}
+
+
+resource "aws_subnet" "private" {
+    cidr_block = cidrsubnet ("${var.vpc_cidr}", 8, 2)
+    vpc_id = aws_vpc.myvpc.id
+    tags = {
+    Name = "private"
+  }
 }
--- providers.tf
+++ providers.tf
@@ -1,9 +1,7 @@
-provider "aws" {
-    alias = "us"
-    region = "us-east-1"
-}
-
-provider "aws" {
-    alias = "europe"
-    region = "eu-central-1"
+terraform {
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+      }
+    }
 }
